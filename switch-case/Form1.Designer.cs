339d0ad2a3d6d8677a57263d8621dc326c384f30
lucas <lucazs.xis@gmail.com>
2025-09-02 10:58:26 -0300

    Add files via upload

---
+++ switch-case/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace switch_case
+{
+    partial class frmSwitch
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.txtCodigoInserido = new System.Windows.Forms.TextBox();
+            this.lblDescricao = new System.Windows.Forms.Label();
+            this.bntenviar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.lblTitulo.Location = new System.Drawing.Point(242, 35);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(229, 24);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Insira o codigo de 1 a 6";
+            this.lblTitulo.Click += new System.EventHandler(this.lblTitulo_Click);
+            // 
+            // txtCodigoInserido
+            // 
+            this.txtCodigoInserido.Location = new System.Drawing.Point(279, 72);
+            this.txtCodigoInserido.Name = "txtCodigoInserido";
+            this.txtCodigoInserido.Size = new System.Drawing.Size(151, 20);
+            this.txtCodigoInserido.TabIndex = 1;
+            // 
+            // lblDescricao
+            // 
+            this.lblDescricao.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblDescricao.Location = new System.Drawing.Point(306, 115);
+            this.lblDescricao.Name = "lblDescricao";
+            this.lblDescricao.Size = new System.Drawing.Size(100, 23);
+            this.lblDescricao.TabIndex = 2;
+            // 
+            // bntenviar
+            // 
+            this.bntenviar.Location = new System.Drawing.Point(319, 152);
+            this.bntenviar.Name = "bntenviar";
+            this.bntenviar.Size = new System.Drawing.Size(75, 23);
+            this.bntenviar.TabIndex = 3;
+            this.bntenviar.Text = "Enviar";
+            this.bntenviar.UseVisualStyleBackColor = true;
+            this.bntenviar.Click += new System.EventHandler(this.bntenviar_Click);
+            // 
+            // frmSwitch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(729, 379);
+            this.Controls.Add(this.bntenviar);
+            this.Controls.Add(this.lblDescricao);
+            this.Controls.Add(this.txtCodigoInserido);
+            this.Controls.Add(this.lblTitulo);
+            this.Name = "frmSwitch";
+            this.Text = "Condicional Switch";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.TextBox txtCodigoInserido;
+        private System.Windows.Forms.Label lblDescricao;
+        private System.Windows.Forms.Button bntenviar;
+    }
+}
+
---
+++ switch-case/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("switch-case")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("switch-case")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("f44b391b-78fe-4a88-99e7-4b4a16739a4d")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
